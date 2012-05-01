33b1999ea0bb5f938a7b56fcda9ff160a82dca19
Adelbert Chang <adelbertc@gmail.com>
2012-05-01 12:58:44 -0700

    Updated Build version from 0.2 to 0.3

--- project/Build.scala
+++ project/Build.scala
@@ -9,7 +9,7 @@ object ProjectBuild extends Build {
     settings = Project.defaultSettings ++ Seq(
       name := "cinema",
       organization := "org.adelbertc",
-      version := "0.2-SNAPSHOT",
+      version := "0.3-SNAPSHOT",
       scalaVersion := "2.9.1",
       resolvers += "Typesafe Releases" at "http://repo.typesafe.com/typesafe/releases",
       libraryDependencies += "com.typesafe.akka" % "akka-actor" % "2.0",
