5f0b55addfa9df33eefea9c448d682df8c3ec502
Adelbert Chang <adelbertc@gmail.com>
2012-05-07 13:53:57 -0700

    added degree dist example

---
+++ src/main/scala/cinema/example/DegreeApp.scala
@@ -0,0 +1,18 @@
+package cinema.example
+
+import cinema.crew._
+import cinema.graph.Graph
+import cinema.graph.immutable.UndirectedGraph
+
+object DegreeApp {
+  def degree(myGraph: Graph, u: Int): Double = myGraph.degree(u)
+
+  def main(args: Array[String]) {
+    if (args.length != 4) {
+      println("Usage: scala HittingTimeApp [edgelist] [subset cardinality] [output filename] [# of servers]")
+      return
+    }
+    val G = new UndirectedGraph(args(0), parallel = true)
+    SingleSourceApp.calculate(G, args(1).toInt, degree, args(2), args(3).toInt)
+  }
+}
